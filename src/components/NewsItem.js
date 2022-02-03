3e880df70287d174d097378d912af15ebfa6e659
Akshay <akshayjee6@gmail.com>
2022-02-03 16:25:09 +0530

    refactoring done

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
                     <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-89296012,width-1070,height-580,imgsize-34568,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":imageUrl} className='card-img-top' alt='...'/>
                         <div className="card-body">
                             <h5 className='card-title'>{title}
-                             <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>{source}</span>
+                             <span className='position-absolute top-0 translate-middle badge rounded-pill bg-danger' style={{left:'83%', zIndex:'1'}}>{source}</span>
                             
                             </h5>
                             <p className='card-text'>{description}...</p>
